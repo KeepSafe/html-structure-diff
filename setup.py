755fb90b94f5cc2f466328110e5f47020eb58f74
Dominic Hayes <dominic@getkeepsafe.com>
2018-08-13 14:17:36 -0700

    Remove usage of PIP internals (https://pip.pypa.io/en/latest/user_guide/#using-pip-from-your-program), these aren't supported in python 3.6.6 (but in 3.6.5).

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 import os
 from setuptools import setup, find_packages
-from pip.download import PipSession
 
-try:
-    from pip._internal.req import parse_requirements
-except ImportError:
-    from pip.req import parse_requirements
 
 version = '0.2.2'
 
@@ -13,12 +8,13 @@ version = '0.2.2'
 def read(f):
     return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
 
-install_reqs = parse_requirements('requirements.txt', session=PipSession())
-reqs = [str(ir.req) for ir in install_reqs]
 
+install_requires = [
+    'mistune <= 1',
+]
 
 setup(name='sdiff',
-      version=version,
+      version='0.2.2',
       description=('sdiff compares the structure of two markdown texts'),
       long_description='\n\n'.join((read('README.md'), read('CHANGELOG'))),
       classifiers=[
@@ -32,5 +28,5 @@ setup(name='sdiff',
       packages=find_packages(exclude=['tests']),
       package_data={},
       namespace_packages=[],
-      install_requires = reqs,
+      install_requires = install_requires,
       include_package_data = False)
